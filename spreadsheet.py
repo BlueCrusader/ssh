d92cd531b599d94c09c90d628b180bdcbb03bd59
Heikki H <heikki.haapaniemi.gml@gmail.com>
2024-10-29 17:28:37 +0200

    US 1 Test 1 & 2

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,8 +1,8 @@
+
 class SpreadSheet:
 
     def __init__(self):
         self._cells = {}
-        self._evaluating = set()
 
     def set(self, cell: str, value: str) -> None:
         self._cells[cell] = value
@@ -11,4 +11,9 @@ class SpreadSheet:
         return self._cells.get(cell, '')
 
     def evaluate(self, cell: str) -> int | str:
-        pass
+        value = self.get(cell)
+        if value.isdigit():
+            return int(value)
+        else:
+            return "#Error"
+
